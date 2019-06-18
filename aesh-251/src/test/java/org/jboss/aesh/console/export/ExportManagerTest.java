1af1ff46f6cc32522058eadf0d4e2cedda7aef35
KomaEc <comaecliptic1997@gmail.com>
2019-06-18 20:54:25 +0800

    update aesh-251

--- aesh-251/src/test/java/org/jboss/aesh/console/export/ExportManagerTest.java
+++ aesh-251/src/test/java/org/jboss/aesh/console/export/ExportManagerTest.java
@@ -19,51 +19,6 @@ import static org.junit.Assert.assertEquals;
  */
 public class ExportManagerTest {
 
-    @Test
-    public void testAddVariable() throws IOException {
-
-        ExportManager exportManager =
-                new ExportManager(new File(Config.getTmpDir()+Config.getPathSeparator()+"aesh_variable_test"));
-
-        exportManager.addVariable("export TEST=/foo/bar");
-        assertEquals("/foo/bar", exportManager.getValue("TEST"));
-        exportManager.addVariable("export FOO=/opt");
-        exportManager.addVariable("export FOO2=$FOO");
-        assertEquals("/opt", exportManager.getValue("$FOO"));
-        assertEquals("/opt", exportManager.getValue("${FOO}"));
-        assertEquals("/opt", exportManager.getValue("FOO2"));
-        assertEquals("/opt", exportManager.getValue("${FOO2}"));
-        assertEquals("/opt:/foo/bar", exportManager.getValue("$FOO:$TEST"));
-        assertEquals("/opt:/foo/bar", exportManager.getValue("$FOO2:${TEST}"));
-        assertEquals("/opt:/foo/bar:/foo", exportManager.getValue("$FOO2:$TEST:/foo"));
-        assertEquals("", exportManager.getValue("$FOO3"));
-
-        exportManager.addVariable("export PATH=$FOO2:$TEST:/foo");
-        exportManager.addVariable("export PATH=$PATH:/bar");
-        assertEquals("/opt:/foo/bar:/foo:/bar", exportManager.getValue("$PATH"));
-        exportManager.addVariable("export FOO2=/bin");
-        assertEquals("/bin", exportManager.getValue("${FOO2}"));
-        assertEquals("/bin:/foo/bar:/foo:/bar", exportManager.getValue("$PATH"));
-
-        exportManager.addVariable("export TEST=/bla /ha");
-        assertEquals("/bla", exportManager.getValue("TEST"));
-
-        assertEquals("ls -la /bla", exportManager.getValue("ls -la $TEST"));
-        assertEquals("/bla ls -la /bla", exportManager.getValue("$TEST ls -la $TEST"));
-    }
-
-    @Test
-    public void testMultipleVariable() throws IOException {
-
-        ExportManager exportManager =
-                new ExportManager(new File(Config.getTmpDir()+Config.getPathSeparator()+"aesh_variable_test"));
-
-        exportManager.addVariable("export FOO=/opt");
-
-        assertEquals("/opt /opt", exportManager.getValue("$FOO $FOO"));
-
-        assertEquals("ls /opt /opt", exportManager.getValue("ls $FOO $FOO"));
-    }
 
     @Test
     public void testVariableNotExist() {
